598a81e3753019ebcbb7960da67e1872fd44ad92
Greta <gretajan09@gmail.com>
2020-04-11 21:39:15 +0300

    failed navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
+import 'react-native-gesture-handler';
 import { NativeRouter, Switch, Route } from 'react-router-native';
-import { View } from 'react-native';
-import { NavigationContainter } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text } from 'react-native';
+// import { NavigationContainter } from "@react-navigation/native";
 import { Provider, connect } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import configureStore from './store';
 const { store, persistor } = configureStore();
 
-import { logOut } from './src/actions/authActions';
+import NavigationComponent from './Navigation';
 
 
 // Components:
@@ -38,57 +37,141 @@ import EditProduct from './componentsAuth/auth_products/EditProduct';
 import SelectedProducts from './components/comparison/selectedProducts';
 
 
+// let AppNavigation = ( { isAuthorized } ) => {
 
-let AppNavigation = () => {
-  const GuestNavigation = createStackNavigator();
-const AuthNavigation = createStackNavigator();
-const Footer = createBottomTabNavigator();
-const AuthFooter = createBottomTabNavigator();
+//   const GuestNavigation = createStackNavigator();
+//   const AuthNavigation = createStackNavigator();
+//   const Footer = createBottomTabNavigator();
+//   const AuthFooter = createBottomTabNavigator();
+
+//   return (
+//         <NavigationContainter>
+//            <GuestNavigation.Navigator>
+//              <GuestNavigation.Screen name="Login" component={LoginPage} />
+//              <GuestNavigation.Screen name="Dashboard" component={Dashboard} options={{ title: Dashboard}} />
+//              <GuestNavigation.Screen name="Subcategories_Auth" component={Subcategories_Auth} />
+//              <GuestNavigation.Screen name="Products_Auth" component={Products_Auth} />
+//              <GuestNavigation.Screen name="Product_Auth" component={Product_Auth} />
+//            </GuestNavigation.Navigator> 
+//            <AuthNavigation.Navigator>
+//                <AuthNavigation.Screen name="Login" component={LoginPage} />
+//                 <AuthNavigation.Screen name="Dashboard" component={Dashboard} options={{ title: Dashboard}} />
+//                <AuthNavigation.Screen name="Subcategories_Auth" component={Subcategories_Auth} />
+//               <AuthNavigation.Screen name="Products_Auth" component={Products_Auth} />
+//              <AuthNavigation.Screen name="Product_Auth" component={Product_Auth} />  
+//            </AuthNavigation.Navigator>
+//            <Footer.Navigator>
+//                <Footer.Screen name="Home" component={Home}/>
+//                <Footer.Screen name="Login" component={LoginPage} />
+//                <Footer.Screen name="SelectedProducts" component={SelectedProducts} />
+//            </Footer.Navigator>
+//         </NavigationContainter>
+//     )
+// }
+
+// class AppNavigation extends Component {
+//   render() {
+//     return (
+//       <View><Text>Text</Text></View>
+//     )
+//   }
+// }
+// let AppNavigation = () => {
+//   const GuestNavigation = createStackNavigator();
+//   const AuthNavigation = createStackNavigator();
+//   const Footer = createBottomTabNavigator();
+//   const AuthFooter = createBottomTabNavigator();
+//     return (
+//       <NavigationContainter>
+//       <AuthNavigation.Navigator>
+//                     <AuthNavigation.Screen name="Login" component={LoginPage} />
+//                        <AuthNavigation.Screen name="Dashboard" component={Dashboard} options={{ title: Dashboard}} />
+//                       <AuthNavigation.Screen name="Subcategories_Auth" component={Subcategories_Auth} />
+//                      <AuthNavigation.Screen name="Products_Auth" component={Products_Auth} />
+//                     <AuthNavigation.Screen name="Product_Auth" component={Product_Auth} />  
+//                   </AuthNavigation.Navigator> 
+//                   </NavigationContainter>
+//     )
+//   }
+
+// const mapStateToProps = (state) => ({
+//   admin: state.auth.admin,
+//   isAuthorized: state.auth.isAuthorized,
+//   selectedProducts: state.selectedProducts.comparisonArray
+
+// })
+
+// AppNavigation = connect(mapStateToProps, {logOut})(AppNavigation)
 
 
-  return (
-        <NavigationContainter>
-          {/* <GuestNavigation.Navigator>
-            <GuestNavigation.Screen name="Login" component={LoginPage} />
-            <GuestNavigation.Screen name="Dashboard" component={Dashboard} options={{ title: Dashboard}} />
-            <GuestNavigation.Screen name="Subcategories_Auth" component={Subcategories_Auth} />
-            <GuestNavigation.Screen name="Products_Auth" component={Products_Auth} />
-            <GuestNavigation.Screen name="Product_Auth" component={Product_Auth} />
-          </GuestNavigation.Navigator> */}
-          <AuthNavigation.Navigator>
-              <AuthNavigation.Screen name="Login" component={LoginPage} />
-              {/* <AuthNavigation.Screen name="Dashboard" component={Dashboard} options={{ title: Dashboard}} />
-              <AuthNavigation.Screen name="Subcategories_Auth" component={Subcategories_Auth} />
-              <AuthNavigation.Screen name="Products_Auth" component={Products_Auth} />
-              <AuthNavigation.Screen name="Product_Auth" component={Product_Auth} /> */}
-          </AuthNavigation.Navigator>
-          <Footer.Navigator>
-              <Footer.Screen name="Home" component={Home} />
-              <Footer.Screen name="Login" component={LoginPage} />
-              <Footer.Screen name="SelectedProducts" component={SelectedProducts} />
-          </Footer.Navigator>
-        </NavigationContainter>
-    )
-}
+// const App = () => {
+//     return (
+//       <Provider store={store}>
+//          <PersistGate loading={null} persistor={persistor}>
+//             <AppNavigation />
+//           </PersistGate>
+//       </Provider>
+//     )
+ 
+// }
 
-const mapStateToProps = (state) => ({
-  admin: state.auth.admin,
-  isAuthorized: state.auth.isAuthorized,
-  selectedProducts: state.selectedProducts.comparisonArray
-})
+// export default App
 
-AppNavigation = connect(mapStateToProps, {logOut})(AppNavigation)
+// class AppNavigation extends Component {
+//   render() {
+//     const GuestNavigation = createStackNavigator();
+//     const AuthNavigation = createStackNavigator();
+//     const Footer = createBottomTabNavigator();
+//     const AuthFooter = createBottomTabNavigator();
+//       return (
+//         <NavigationContainter>
+//           <GuestNavigation.Navigator>
+//           <GuestNavigation.Screen name="Login" component={LoginPage} />
+//               <GuestNavigation.Screen name="Dashboard" component={Dashboard} options={{ title: Dashboard}} />
+//             <GuestNavigation.Screen name="Subcategories_Auth" component={Subcategories_Auth} />
+//             <GuestNavigation.Screen name="Products_Auth" component={Products_Auth} />
+//           <GuestNavigation.Screen name="Product_Auth" component={Product_Auth} />  
+//         </GuestNavigation.Navigator> 
+//         </NavigationContainter>
+
+//       )
+//   }
+ 
+//   }
+// export default AppNavigation
+
+// const GuestNavigation = createStackNavigator();
+// const AuthNavigation = createStackNavigator();
+// const Footer = createBottomTabNavigator();
+// const AuthFooter = createBottomTabNavigator();
+
+// const AppNavigation = () => {
+//       return (
+//         <Provider store={store}>
+//          <PersistGate loading={null} persistor={persistor}>
+//         <NavigationContainter>
+//           <GuestNavigation.Navigator>
+//           <GuestNavigation.Screen name="Login" component={LoginPage} />
+//               <GuestNavigation.Screen name="Dashboard" component={Dashboard} options={{ title: Dashboard}} />
+//             <GuestNavigation.Screen name="Subcategories_Auth" component={Subcategories_Auth} />
+//             <GuestNavigation.Screen name="Products_Auth" component={Products_Auth} />
+//           <GuestNavigation.Screen name="Product_Auth" component={Product_Auth} />  
+//         </GuestNavigation.Navigator> 
+//         </NavigationContainter>
+//         </PersistGate>
+//         </Provider>
+
+//       )
+//   }
+// export default AppNavigation
 
 const App = () => {
-
-    return (
-      <Provider store={store}>
-         <PersistGate loading={null} persistor={persistor}>
-            <AppNavigation />
-          </PersistGate>
-      </Provider>
-    )
-  }
-
-export default App
-
+        return (
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <NavigationComponent />
+            </PersistGate>
+          </Provider>
+        )
+    }
+  export default App
