ee46144d6a9ae31d27b3b13e1d68fc638444500d
GretaJan <gretajan09@gmail.com>
2020-09-09 22:06:35 +0300

    Created Jest test to check categories api fetch

--- frontend/App.js
+++ frontend/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { Provider, connect } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
-import configureStore from './src/redux/store';
 const { store, persistor } = configureStore();
+import configureStore from './src/redux/store';
+
 
 import Tabs from './src/components_additional/Navigation/Tabs';
 
